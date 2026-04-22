167bf528ecd2cad100ea9f257bc62cc889033f2f
Shinyyyy <ShinyGajula@sudheers-MacBook-Pro.local>
2026-04-21 22:46:15 -0700

    first iteration of code done

--- scraper.py
+++ scraper.py
@@ -1,9 +1,73 @@
 import re
-from urllib.parse import urlparse, RobotFileParser
+from urllib.parse import urlparse, RobotFileParser, urldefrag
 import urllib.robotparser
 from bs4 import BeautifulSoup
+from collections import defaultdict
+
+
+
+freqWords = dict()
+longestPageCnt, longestPage = 0, None
+UniquePages = set()
+subDomainFreq = defaultdict(set)
+
+stopWords = {
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
+    "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being",
+    "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't",
+    "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during",
+    "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't",
+    "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here",
+    "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i",
+    "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it",
+    "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my",
+    "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or",
+    "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same",
+    "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so",
+    "some", "such", "than", "that", "that's", "the", "their", "theirs", "them",
+    "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll",
+    "they're", "they've", "this", "those", "through", "to", "too", "under",
+    "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're",
+    "we've", "were", "weren't", "what", "what's", "when", "when's", "where",
+    "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with",
+    "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've",
+    "your", "yours", "yourself", "yourselves"
+}
 
 def scraper(url, resp):
+    if not is_valid(url):
+        return []
+
+    print("url: ", url)
+    
+    # open html file and lowercase the words and split in array
+    resp = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    text = resp.get_text().lower().split(" ")
+    
+    # getting frequency of words, exluding stop words
+    for word in text:
+        if word not in stopWords:
+            freqWords[word] = freqWords.get(word,0) + 1
+    
+    # finding longest page
+    if len(text) > longestPageCnt:
+        longestPage = url
+        longestPageCnt = len(text)
+
+    # finding all unique pages
+    clean_url, _ = urldefrag(url)
+    UniquePages.add(clean_url)
+
+    # finding subdomains
+    parsed = urlparse(url)
+    subdomain = parsed.netloc
+    subDomainFreq[subdomain].add(urldefrag(url))
+
+    
+
+
+
+    
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
